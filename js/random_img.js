330026509e4266c23b77aa623e9105dc5c4aa88b
Gabriel Urbain <gabrielurbain@gmail.com>
2018-01-22 11:31:31 +0100

    test

--- js/random_img.js
+++ js/random_img.js
@@ -1,17 +1,1 @@
 var urlstr = "http://irobotique.be:9088/"
-
-$.ajax({
-            url: urlstr,
-            type: "GET",
-            crossDomain: true,
-            xhrFields: {
-                withCredentials: false
-            },
-            success: function (response) {
-              console.log("PhD comics img found on " + urlstr);
-              $('#img_phd').html(response);
-            },
-            error: function (xhr, status) {
-              console.log("Error: PhD comics img not found. Check the server..");
-            }
-        });
